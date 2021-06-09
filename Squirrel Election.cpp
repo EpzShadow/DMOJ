05413a7ed5d8b3237b8131402bee207cba8983fc
EpzShadow <85049418+EpzShadow@users.noreply.github.com>
2021-06-09 19:57:43 -0400

    Add files via upload

---
+++ Squirrel Election.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+	long long n, sum = 0;
+	cin >> n;
+	vector<pair<long long, long long>>arr(n);
+	vector<long long>dp(5001, INT_MAX);
+	for (int i = 0; i < n; i++) {
+		long long x, y;
+		cin >> x >> y;
+		sum += y;
+		arr[i] = make_pair(x, y);
+	}
+	dp[0] = 0;
+	//dp[i] is min voters for i points
+	for (int j = 0; j < n; j++) {
+		for (int i = sum; i >= 0; i--) {
+			long long c = arr[j].first / 2 + 1;
+			dp[i + arr[j].second] = min(dp[i] + c, dp[i + arr[j].second]);
+		}
+	}
+	long long x = sum / 2 + 1;
+	//cout << dp[x];
+	for (int i = x; i <= sum; i++) {
+		if (dp[i] != INT_MAX) {
+			cout << dp[i] << endl;
+			break;
+		}
+	}
+	//cout << dp[sum / 2 + 1] << endl;
+}
