5af9803813078cf837df2c68dbdaafb628496201
EpzShadow <85049418+EpzShadow@users.noreply.github.com>
2021-08-10 14:44:21 -0400

    Add files via upload

---
+++ Sound.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+const int MM = 1e6 + 2;
+int arr[MM];
+int main(){
+    deque<int> mi, mx;
+    vector<int> ans;
+    int n, m, c;
+    cin >> n >> m >> c;
+    for(int i = 0; i < n; i ++){
+
+    }
+}
